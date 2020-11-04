3b509accc82639f3bdd168d6a736d9011c485759
bruno yuji yoshioka <bruno.yuji1995@gmail.com>
2020-11-04 17:42:06 -0300

    Criando o componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', // caminho url raiz
+  component: HomeComponent // roteamento para que toda vez que acessar a url raiz
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
